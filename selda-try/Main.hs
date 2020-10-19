b7629851fe2f4ed6ff2edc15d568ebcfb2770813
kamoii <>
2020-10-20 02:11:13 +0900

    Improve notNull constraint

--- selda-try/Main.hs
+++ selda-try/Main.hs
@@ -23,6 +23,7 @@ import qualified Database.Selda.SQLite as Selda
 
 import Database.Nelda.Schema
 import Database.Nelda.Schema.ColumnType as T
+import Database.Nelda.Schema.ColumnConstraintCommon
 import Database.Nelda.SqlTypeDeriveStrategy as SqlTypeDeriving
 import qualified Database.Nelda.Query.Select as Nelda
 import qualified Database.Nelda.Action as Nelda
@@ -130,7 +131,7 @@ data Pet = Dog | Horse | Dragon
 -- people :: _
 people = table #people
     ( column #name T.text & notNull
-    , column #age  T.int  & notNull
+    , column #age  T.int  & notNull & notNull
     , column #pet  (T.text & asSqlType @Pet) & default_ Dog
     )
 
@@ -139,7 +140,7 @@ data People2 = People2
     , age :: Int
     } deriving Generic
 
-test :: IO _
+test :: IO ()
 test = withSQLite "people.sqlite" $ do
 
     -- createTable people'
@@ -158,7 +159,7 @@ test = withSQLite "people.sqlite" $ do
         row <- Nelda.select people
         restrict $ row.age .>= 18
         pure row
-
+    pure ()
 
 -- TODO: pPrint の出力がコンパクトになるように調整したい
 main :: IO ()
--- selda/src/Database/Nelda/Schema/Column.hs
+++ selda/src/Database/Nelda/Schema/Column.hs
@@ -49,22 +49,6 @@ column colName colType =
            , constraintDefault = Nothing
            }
 
--- TODO: これは Nullable じゃないときに notNull しようとした場合のエラーメッセージを改善できるかな...
-notNull
-    :: Column _name _columnType _sqlType 'Nullable _default
-    -> Column _name _columnType _sqlType 'NotNull _default
-notNull c = c { constraintNotNull = True }
-
--- TODO: エラーメッセージの改善
--- TODO: defualt_ という名前のほうがいいかな？
--- TODO: 関数呼びだし系の DEAFULT もあるので unsafeDefault も必要かな？
-default_
-    :: SqlType sqlType
-    => sqlType
-    -> Column _name _columnType sqlType _nullability 'NoDefault
-    -> Column _name _columnType sqlType _nullability 'ExplicitDefault
-default_ v c = c { constraintDefault = Just $ toSqlExpression v }
-
 -- | SqlColumnType によって基本 対応する SqlType が決まるが,互換性のあるSqlType に変えたい場合。
 -- 互換性のある,というのは ToSqlType ct ~ OriginSqlType st' という条件で確認している。
 --
