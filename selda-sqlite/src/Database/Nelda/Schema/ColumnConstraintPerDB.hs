b11a404bdeb2330b76c3c32e504707235cfc1902
kamoii <>
2020-10-20 02:16:40 +0900

    [WIP] Column constraints

---
+++ selda-sqlite/src/Database/Nelda/Schema/ColumnConstraintPerDB.hs
@@ -0,0 +1,7 @@
+module Database.Nelda.Schema.ColumnConstraintPerDB where
+
+
+-- * PRIMARY
+--
+-- DB毎に性質が異なる.
+
---
+++ selda/src/Database/Nelda/Schema/ColumnConstraint.hs
@@ -0,0 +1,7 @@
+module Database.Nelda.Schema.ColumnConstraint
+    ( module Database.Nelda.Schema.ColumnConstraintCommon
+    , module Database.Nelda.Schema.ColumnConstraintPerDB
+    ) where
+
+import Database.Nelda.Schema.ColumnConstraintCommon
+import Database.Nelda.Schema.ColumnConstraintPerDB
