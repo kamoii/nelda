cae5506a535d285eba341d2f4a0c943e50104fb0
kamoii <>
2020-11-09 22:18:58 +0900

    Remove selda-core package

--- nelda-sqlite/src/Database/Nelda/Types.hs
+++ nelda-sqlite/src/Database/Nelda/Types.hs
@@ -5,12 +5,5 @@ module Database.Nelda.Types where
 import Data.Text (Text)
 import Data.String (IsString)
 
--- * SqlFragment
--- CREATE文も SQLの一部だよ。
-
-newtype SqlFragment = SqlFragment Text
-    deriving (Show, Eq, IsString, Semigroup, Monoid)
-
-
 newtype Sql = Sql Text
     deriving (Show, Eq, IsString, Semigroup, Monoid)
--- selda/src/Database/Nelda/Schema.hs
+++ selda/src/Database/Nelda/Schema.hs
@@ -1,24 +1,19 @@
-module Database.Nelda.Schema
-    ( module Database.Nelda.Schema.Types
-    , module Database.Nelda.Schema.Column
-    , module Database.Nelda.Schema.ColumnConstraint
-    , module Database.Nelda.Schema.IsColumnNames
-    , module Database.Nelda.Schema.IsColumnSubset
-    , module Database.Nelda.Schema.IsColumns
-    , module Database.Nelda.Schema.Table
-    , module Database.Nelda.Schema.TableConstraint
-    , module Database.Nelda.Schema.Index
-    ) where
+module Database.Nelda.Schema (
+    module Database.Nelda.Schema.Column,
+    module Database.Nelda.Schema.ColumnConstraint,
+    module Database.Nelda.Schema.IsColumnNames,
+    module Database.Nelda.Schema.IsColumnSubset,
+    module Database.Nelda.Schema.IsColumns,
+    module Database.Nelda.Schema.Table,
+    module Database.Nelda.Schema.TableConstraint,
+    module Database.Nelda.Schema.Index,
+) where
 
--- from cores
-import Database.Nelda.Schema.Types
-
---
 import Database.Nelda.Schema.Column
 import Database.Nelda.Schema.ColumnConstraint
+import Database.Nelda.Schema.Index
 import Database.Nelda.Schema.IsColumnNames
 import Database.Nelda.Schema.IsColumnSubset
 import Database.Nelda.Schema.IsColumns
 import Database.Nelda.Schema.Table
 import Database.Nelda.Schema.TableConstraint
-import Database.Nelda.Schema.Index
